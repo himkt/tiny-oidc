3564c0d753ae96bb46e272a8dfb032c4127bac0b
himkt <himkt@klis.tsukuba.ac.jp>
2024-03-11 20:04:13 +0900

    feat: init server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{
+    response::IntoResponse, routing::{get, post}, Router
+};
+
+async fn login() -> impl IntoResponse {
+    {}
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/login", post(login))
+        .route("/openid-connect/auth", get(|| async { {} }))
+        .route("/openid-connect/token", post(|| async { {} }))
+        .route("/openid-connect/introspect", post(|| async { {} }))
+        .route("/openid-connect/jwks.json", get(|| async { {} }))
+        .route("/openid-connect/.well-known/openid-configuration", get(|| async { {} }))
+        ;
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
